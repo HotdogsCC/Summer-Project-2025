2362ecdd92419071b690ec4477e0412dd32e07e2
JamblesWoomy <jmcampbell1309@gmail.com>
2025-02-07 21:54:20 +1100

    Fixed some things

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class buk : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            print("Hit " + collision.gameObject.name + " !");
+            Destroy(gameObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
