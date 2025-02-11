9ea2f57d29e1a8bd64e32a8f3e6b3f5c1df223e1
JamblesWoomy <jmcampbell1309@gmail.com>
2025-02-11 19:02:41 +1100

    Fixed direction jump bug

--- Assets/Scripts/MouseMovementX.cs
+++ Assets/Scripts/MouseMovementX.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseMovement : MonoBehaviour
+public class MouseMovementX : MonoBehaviour
 {
-
     public float mouseSensitivity = 600f;
 
     float xRotation = 0f;
     float yRotation = 0f;
-
-    public float topClamp = -90f;
-    public float bottomClamp = 90f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,18 +22,10 @@ public class MouseMovement : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //X axis rotation
-        xRotation -= mouseY;
-
-        //Clamp rotation
-        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
-
         //Y axis rotation
         yRotation += mouseX;
 
         //Apply rotation
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-
-
     }
 }
